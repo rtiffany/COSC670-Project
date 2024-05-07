200afb559a7ac508fe47d69ac76ec75a32ad48fa
devereaweaver <deveredev@proton.me>
2024-05-07 14:46:40 -0400

    Implemented React component to allow users to vote in a given poll. Need to add the submission button and send the data off to the chain.

--- client/src/components/Vote/VotePage.jsx
+++ client/src/components/Vote/VotePage.jsx
@@ -1,88 +1,81 @@
 import * as React from 'react';
 import '../Login/Login.css';
 import './vote.css'
-import LinkButton from '../LinkButton/LinkButton';
-import InputWithLabel from '../InputWithLabel/InputWithLabel';
-import { ethers } from 'ethers';
+// import LinkButton from '../LinkButton/LinkButton';
+// import InputWithLabel from '../InputWithLabel/InputWithLabel';
+import { AbstractProvider, ethers } from 'ethers';
 import abi from '../../contracts/Poll.json'
 import contractAddress from '../../contracts/contractAddress.json';
+import { act } from 'react';
+
 
 export default function VotePage() {
 
-    const [provider, setProvider] = React.useState(null);
+    const [provider, setProvider] = React.useState();
+    const [pollName, setPollName] = React.useState();
+    const [pollDescription, setPollDescription] = React.useState();
+    const [candidateState, setCandidateState] = React.useState([]);
+    const [selectedValue, setSelectedValue] = React.useState();
+
+    const handleRadioChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
+
+    const GetData = async () => {
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(contractAddress.Poll, abi.abi, signer);
+        const [name, description, candidates] = await contract.getPollDetails('TestPoll3');
+        setPollName(name);
+        setPollDescription(description);
+        setCandidateState(candidates);
+
+
+        // console.log('Initial page render')
+        // console.log(`Name: ${pollName}`);
+        // console.log(`Description: ${pollDescription}`);
+        // console.log(`Candidates: ${candidateState}`);
+        // // activePolls = await contract.getLivePolls();
+        // console.log("Currently Active Polls")
+        // for (let i = 0; i < activePolls.length; i++) {
+        // console.log(activePolls[i][1])
+        // }
+    }
 
 
     React.useEffect(() => {
         if (typeof window !== "undefined") {
-          if (window.ethereum) {
-            setProvider(new ethers.BrowserProvider(window.ethereum));
-            // getCurrentAd()
-          } else {
-            console.error("Please install MetaMask!");
-          }
+            if (window.ethereum) {
+                setProvider(new ethers.BrowserProvider(window.ethereum));
+            } else {
+                console.error("Please install MetaMask!");
+            }
         }
-      }, []);
 
-   
+        GetData();
+    }, []);
 
     return (
-        <div>
-            <div className='d-flex vh-100 bg-dark-grey justify-content-center align-items-center content'>
-                <div className='login-form col-md-5' style={{ margin: '20px 100px', display: 'flex', flexDirection: 'column', paddingTop: '20px' }}>
-                    <form style={{ flex: 1 }}>
-                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                            <div style={{ flex: 1 }}>
-                                <h2>Poll 1:</h2>
-                                <div className='mb-4' style={{ display: 'flex', alignItems: 'center' }}>
-                                    <h3 style={{ margin: '0', marginRight: '10px' }}>Poll Name</h3>
-                                    
-                                </div>
-                                <br />
-                                <div className='mb-4' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                    <label style={{ margin: '0', marginRight: '10px' }}>Poll Desc.</label>
-                                    <input
-                                        type='text'
-                                        placeholder='Poll Description'
-                                        className='form-control'
-                                        style={{ textAlign: 'center' }}
-                                    />
-                                </div>
-                                <br />
-                                <div className='mb-4' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                    <label style={{ margin: '0', marginRight: '10px' }}>End Time</label>
-                                    <input
-                                        type='time'
-                                        placeholder='End Time'
-                                        className='form-control'
-                                        style={{ textAlign: 'center' }}
-                                    />
-                                </div>
-                                <br />
-                                <div className='mb-4' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                    <label style={{ margin: '0', marginRight: '10px' }}>End Date</label>
-                                    <input
-                                        type='date'
-                                        placeholder=''
-                                        className='form-control'
-                                        style={{ textAlign: 'center' }}
-                                    />
-                                </div>
-                                <div>
-                                     <LinkButton to="/Results" text="Submit" />
-                                </div>
-                            </div>
-                            <br />
-                        </div>
-                    </form>
-                </div>
-                {/* <div className='livePoll'>
-                    <h2>Live Polls</h2>
-                    <div className='buttonContainer'>
-                        <button type="submit" className='btn btn-success w-100'>Poll 1</button>
-                        <button type="submit" className='btn btn-success w-100'>Poll 2</button>
-                    </div>
-                </div> */}
-            </div>
-        </div>
-    );
+        <>
+            <h2>Poll name: {pollName}</h2>
+            <h3>Poll Description: {pollDescription}</h3>
+            <h3>Candidates:</h3>
+            {
+                candidateState.map((candidate, index) => (
+                    <>
+                        <input
+                            type='radio'
+                            value={candidate[0]}
+                            name='option'
+                            key={index}
+                            onChange={handleRadioChange}
+                        />
+                        <label>{candidate[0]}</label>
+                        <br/>
+                    </>
+                ))
+            }
+            {console.log("Current selection:" + selectedValue)}
+        </>
+    )
 }
